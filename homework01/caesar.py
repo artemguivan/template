d28dee17ba2ee4e9143f203dafd5621e823e8c5a
artemguivan <94199223+artemguivan@users.noreply.github.com>
2024-12-11 08:39:35 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,46 +1,42 @@
-import typing as tp
-
-
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
+    z_number = 122
+    a_number = 97
+    Z_number = 90
+    A_number = 65
 
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
-    return ciphertext
+    ciphertext = []
+    for char in plaintext:
+        if char.isalpha():
+            if char.isupper():
+                ord_of_char = ord(char) + shift
+                cycle_num = ord_of_char - Z_number + A_number - 1
+                ciphertext.append(chr(ord_of_char) if ord_of_char <= Z_number else chr(cycle_num))
+            else:
+                ord_of_char = ord(char) + shift
+                cycle_num = ord_of_char - z_number + a_number - 1
+                ciphertext.append(chr(ord_of_char) if ord_of_char <= z_number else chr(cycle_num))
+        else:
+            ciphertext.append(char)
+    return "".join(ciphertext)
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
+    z_number = 122
+    a_number = 97
+    Z_number = 90
+    A_number = 65
 
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
-    plaintext = ""
-    # PUT YOUR CODE HERE
-    return plaintext
-
-
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
-    best_shift = 0
-    # PUT YOUR CODE HERE
-    return best_shift
+    plaintext = []
+    for char in ciphertext:
+        if char.isalpha():
+            if char.isupper():
+                ord_of_char = ord(char) - shift
+                cycle_num = Z_number - (A_number - ord_of_char) + 1
+                plaintext.append(chr(ord_of_char) if ord_of_char >= A_number else chr(cycle_num))
+            else:
+                ord_of_char = ord(char) - shift
+                cycle_num = z_number - (a_number - ord_of_char) + 1
+                plaintext.append(chr(ord_of_char) if ord_of_char >= a_number else chr(cycle_num))
+        else:
+            plaintext.append(char)
+    return "".join(plaintext)
